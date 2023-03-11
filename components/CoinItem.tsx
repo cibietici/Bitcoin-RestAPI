1fe081df7ef371367a1aff21a53cfc645704c2d8
carlo alberto burato <carloalberto.burato@gmail.com>
2023-03-11 12:10:45 +0100

    Fix mobile visualization issue

--- components/CoinItem.tsx
+++ components/CoinItem.tsx
@@ -8,6 +8,9 @@ export default function CoinItem( props: {coin: Coin}) {
       <time aria-label='verdi dato'>
         {convertDate(props.coin.time)}
       </time>
+      <span aria-label='konverteringstype'>
+        {props.coin.conversionType}
+      </span>
       <span aria-label='tap verdi'>
         <Image src='/down.svg' width={20} height={12} alt='' aria-hidden="true" />
         {props.coin.low}
@@ -15,9 +18,6 @@ export default function CoinItem( props: {coin: Coin}) {
       <span aria-label='opptjent verdi'>
       <Image src='/up.svg' width={20} height={12} alt='' aria-hidden="true" />
         {props.coin.high}
-      </span>
-      <span aria-label='konverteringstype'>
-        {props.coin.conversionType}
       </span>
     </li>
 }
